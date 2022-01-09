5a0b5b1b051e873577c2c7f358a3d80dfbc0f1ae
justin-hill426 <jrhill426@gmail.com>
2022-01-08 22:52:32 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(MyApp());
 }
 
+// extending the StatelessWidget class in order to use basic flutter functionality
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,3 +12,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
