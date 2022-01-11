e2bb692cfac4d152d9867d725ec2702245f080aa
justin-hill426 <jrhill426@gmail.com>
2022-01-10 21:23:20 -0700

    Added statetracking to widget class

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,51 @@ void main() {
 }
 
 // extending the StatelessWidget class in order to use basic flutter functionality
-class MyApp extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Text('Hello!'),
-    );
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
 }
 
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
